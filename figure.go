1c73c90bbaabf48541f1aa3f346b67db154ac6ee
Bob Glickstein <bobg@emphatic.com>
2019-08-12 18:26:30 -0700

    Use the wikipedia link as a unique key for figures. Remove the idea of timestamped "generations."

--- figure.go
+++ figure.go
@@ -2,26 +2,26 @@ package outlived
 
 import (
 	"context"
-	"encoding/binary"
 	"fmt"
 	"log"
 	"sort"
 	"time"
 
 	"cloud.google.com/go/datastore"
-	"github.com/bobg/aesite"
 	"github.com/pkg/errors"
 	"google.golang.org/api/iterator"
 )
 
 // Figure is a historical figure.
 type Figure struct {
-	Name, Desc, Link string
-	Born, Died       Date
-	DaysAlive        int
-	Pageviews        int
+	// Link is the path part of the figure's Wikipedia URL.
+	// This also serves as the figure's unique datastore key.
+	Link string
 
-	TimestampMS int64
+	Name, Desc string
+	Born, Died Date
+	DaysAlive  int
+	Pageviews  int
 }
 
 func (f *Figure) YDAge() string {
@@ -48,23 +48,18 @@ func (f *Figure) YDAge() string {
 	return fmt.Sprintf("%s, %s", ystr, dstr)
 }
 
-func FiguresAliveFor(ctx context.Context, client *datastore.Client, days int) ([]*Figure, error) {
-	q, err := figureQuery(ctx, client)
-	if err != nil {
-		return nil, errors.Wrap(err, "constructing query")
+func FiguresAliveFor(ctx context.Context, client *datastore.Client, days, limit int) ([]*Figure, error) {
+	q := datastore.NewQuery("Figure").Filter("DaysAlive =", days).Order("-Pageviews")
+	if limit > 0 {
+		q = q.Limit(limit)
 	}
-	q = q.Filter("DaysAlive =", days).Order("-Pageviews")
 	var figures []*Figure
-	_, err = client.GetAll(ctx, q, &figures)
+	_, err := client.GetAll(ctx, q, &figures)
 	return figures, errors.Wrap(err, "querying figures")
 }
 
 func FiguresAliveForAtMost(ctx context.Context, client *datastore.Client, days, limit int) ([]*Figure, error) {
-	q, err := figureQuery(ctx, client)
-	if err != nil {
-		return nil, errors.Wrap(err, "constructing query")
-	}
-	q = q.Filter("DaysAlive <=", days).Order("-DaysAlive").Order("-Pageviews")
+	q := datastore.NewQuery("Figure").Filter("DaysAlive <=", days).Order("-DaysAlive").Order("-Pageviews")
 	it := client.Run(ctx, q)
 	var figures []*Figure
 	for len(figures) < limit {
@@ -85,39 +80,46 @@ func FiguresAliveForAtMost(ctx context.Context, client *datastore.Client, days, 
 }
 
 func FiguresDiedOn(ctx context.Context, client *datastore.Client, mon time.Month, day int, limit int) ([]*Figure, error) {
-	q, err := figureQuery(ctx, client)
-	if err != nil {
-		return nil, errors.Wrap(err, "constructing query")
-	}
-	q = q.Filter("Died.M =", int(mon)).Filter("Died.D =", day).Order("-Pageviews")
+	q := datastore.NewQuery("Figure").Filter("Died.M =", int(mon)).Filter("Died.D =", day).Order("-Pageviews")
 	if limit > 0 {
 		q = q.Limit(limit)
 	}
 	var figures []*Figure
-	_, err = client.GetAll(ctx, q, &figures)
+	_, err := client.GetAll(ctx, q, &figures)
 	return figures, errors.Wrap(err, "querying figures")
-}
-
-func figureQuery(ctx context.Context, client *datastore.Client) (*datastore.Query, error) {
-	timestampMSBytes, err := aesite.GetSetting(ctx, client, "generation")
-	if err != nil {
-		return nil, errors.Wrap(err, "getting generation setting")
-	}
-	timestampMS, n := binary.Varint(timestampMSBytes)
-	if n <= 0 {
-		return nil, errors.Wrapf(err, "decoding generation setting %x", timestampMSBytes)
-	}
-	return datastore.NewQuery("Figure").Filter("TimestampMS = ", timestampMS), nil
 }
 
 const multiLimit = 500
 
 func ReplaceFigures(ctx context.Context, client *datastore.Client, figures []*Figure) error {
-	nowMS := time.Now().UnixNano() * int64(time.Nanosecond) / int64(time.Millisecond)
+	beforeDeduping := len(figures)
+
+	// Remove duplicates from figures.
+	var (
+		seen    = make(map[string]struct{})
+		deduped []*Figure
+	)
+	for _, fig := range figures {
+		if _, ok := seen[fig.Link]; ok {
+			continue
+		}
+		seen[fig.Link] = struct{}{}
+		deduped = append(deduped, fig)
+	}
+	figures = deduped
+
+	afterDeduping := len(figures)
+
+	allQ := datastore.NewQuery("Figure")
+
+	before, err := client.Count(ctx, allQ)
+	if err != nil {
+		return errors.Wrap(err, "counting figures before replace")
+	}
+
 	keys := make([]*datastore.Key, len(figures))
-	for i := range figures {
-		keys[i] = &datastore.Key{Kind: "Figure"}
-		figures[i].TimestampMS = nowMS
+	for i, fig := range figures {
+		keys[i] = &datastore.Key{Kind: "Figure", Name: fig.Link}
 	}
 	for len(figures) > 0 {
 		var (
@@ -134,32 +136,13 @@ func ReplaceFigures(ctx context.Context, client *datastore.Client, figures []*Fi
 		}
 		keys, figures = nextKeys, nextFigs
 	}
-	var nowMSBytes [binary.MaxVarintLen64]byte
-	n := binary.PutVarint(nowMSBytes[:], nowMS)
-	err := aesite.SetSetting(ctx, client, "generation", nowMSBytes[:n])
+
+	after, err := client.Count(ctx, allQ)
 	if err != nil {
-		return errors.Wrap(err, "finalizing figure storage")
-	}
-	q := datastore.NewQuery("Figure").Filter("TimestampMS <", nowMS).KeysOnly()
-	keys, err = client.GetAll(ctx, q, nil)
-	if err != nil {
-		// Log but otherwise ignore this error.
-		log.Printf("querying stale figures with TimestampMS < %d: %s", nowMS, err) // xxx use "appengine/log".Warningf when in the appengine context?
-		return nil
+		return errors.Wrap(err, "counting figures after replace")
 	}
 
-	for len(keys) > 0 {
-		var nextKeys []*datastore.Key
-		if len(keys) > multiLimit {
-			keys, nextKeys = keys[:multiLimit], keys[multiLimit:]
-		}
-		err = client.DeleteMulti(ctx, keys)
-		if err != nil {
-			// Log but otherwise ignore this error.
-			log.Printf("deleting %d stale figures with TimestampMS < %d: %s", len(keys), nowMS, err) // xxx use "appengine/log".Warningf when in the appengine context?
-			break
-		}
-		keys = nextKeys
-	}
+	log.Printf("replaced figures, %d before deduping, %d after; %d in db before, %d after", beforeDeduping, afterDeduping, before, after)
+
 	return nil
 }
