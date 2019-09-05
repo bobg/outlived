11519bc2051bef45af62bc5c30ce01a5b280b76e
Bob Glickstein <bobg@emphatic.com>
2019-09-04 19:07:58 -0700

    Checkpoint.

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -2,25 +2,39 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-const App: React.FC = () => {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+interface Props {
+}
+
+interface State {
+  data: any // xxx
+}
+
+class App extends React.Component{
+  constructor(props) {
+    super(props)
+    this.getData()
+  }
+
+  private getData = async () => {
+    // xxx server request
+    this.setState({data})
+  }
+
+  public render() {
+    const { data } = this.state
+
+    return (
+        <div className="App">
+        <header className="App-header">
+        <img src={logo} className="App-logo" alt="Outlived" />
+        </header>
+        {data.user && (
+            <SignedIn user={data.user} />
+        )}
+        <Figures figures={data.figures} user={data.user} />
+      </div>
+    );
+  }
 }
 
 export default App;
