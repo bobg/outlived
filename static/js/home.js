23b4aac88c48b22a20a8e3621dd7dfdee73fcf2f
Bob Glickstein <bobg@emphatic.com>
2019-09-11 16:03:38 -0700

    Adjust to new Figure.Link format (i.e., without the leading /wiki/). Add an admin command for migrating existing Figures.

--- static/js/home.js
+++ static/js/home.js
@@ -129,7 +129,7 @@ $(document).ready(function() {
 
       resp.Figures.forEach(figure => {
         var liEl = $('<li></li>');
-        var aEl = $('<a target="_blank"></a>').attr('href', 'https://en.wikipedia.org' + figure.Link);
+        var aEl = $('<a target="_blank"></a>').attr('href', 'https://en.wikipedia.org/wiki/' + figure.Link);
         if (figure.ImgSrc) {
           aEl.append($('<img class="img64">').attr('src', 'https:' + figure.ImgSrc).attr('alt', figure.ImgAlt));
           aEl.append($('<br>'));
