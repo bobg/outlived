41a09fcf9ae9e07d846cf4389ce71a1910b4907a
Bob Glickstein <bobg@emphatic.com>
2019-09-25 07:19:26 -0700

    "bootstrap" css

--- web/src/Figures.tsx
+++ web/src/Figures.tsx
@@ -28,14 +28,14 @@ export class Figures extends React.Component<Props, State> {
     if (user) {
       return (
         <Tabs
-          id='figures'
+          id="figures"
           activeKey={this.state.activeTab}
           onSelect={this.handleTab}
         >
-          <Tab eventKey={1} title='Died on this date'>
+          <Tab eventKey={1} title="Died on this date">
             {renderFigs(figures, true)}
           </Tab>
-          <Tab eventKey={2} title='You have recently outlived'>
+          <Tab eventKey={2} title="You have recently outlived">
             {renderFigs(user.figures, false)}
           </Tab>
         </Tabs>
@@ -52,18 +52,18 @@ export class Figures extends React.Component<Props, State> {
 }
 
 const renderFigs = (figs: FigureData[], showAge: boolean) => (
-  <ul className='grid'>
+  <ul className="grid">
     {figs.map((fig: FigureData) => (
       <li>
         <a
-          className='figure'
-          target='_blank'
-          rel='noopener noreferrer'
+          className="figure"
+          target="_blank"
+          rel="noopener noreferrer"
           href={fig.href}
         >
           {fig.imgSrc && (
             <span>
-              <img className='img64' src={fig.imgSrc} alt={fig.imgAlt} />
+              <img className="img128" src={fig.imgSrc} alt={fig.imgAlt} />
               <br />
             </span>
           )}
